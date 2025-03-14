65c3d11ad92284989e98ec65f3c6fc5ae6073b25
Rinishulhah <shulhahrini@gmail.com>
2025-03-15 00:49:18 +0700

    Tugas 2 js

---
+++ Konversi Fahrenheit ke Celsius1.js
@@ -0,0 +1,6 @@
+// 1. konversi Fahrenheit ke Celcius 
+function fahrenheitTocelcius(farenheit) { 
+    return (farenheit-32) * 5 / 9; 
+    }
+    console.log(farenheitToCelcius(100)); // Output: 37.777...
+
